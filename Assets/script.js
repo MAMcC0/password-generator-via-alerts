0ab12679ac2d2920f16e42994a7b76f7310d2a36
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-05-27 18:00:43 -0700

    added descriptive comments for variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+//variables for lowercase & uppercase letters, special characters, and numbers
 var lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
   'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
   'w', 'x', 'y', 'z'];
