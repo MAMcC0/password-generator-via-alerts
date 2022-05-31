854d8cf514aaedce57bb6c213d53a9ffb25112b0
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-05-30 18:37:54 -0700

    moved password.text value to bottom to ensure it displayed

--- Assets/script.js
+++ Assets/script.js
@@ -23,12 +23,12 @@ var userPassword = [];
 function writePassword() {
   //prompt for how many characters to add
   var inputCharacters = parseInt(prompt('How many characters would you like in your password (between 8-128 characters required)',''),10);
-  // var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
-  //add parseInt to check if it is a number, then check if its between 8-128, push alerts if not
-  passwordText.value = password;
+  
+ 
 
-
+console.log(passwordText);
   function charCheck() {
     if (!inputCharacters) {
       alert("Oops! Please put a number between 8-128 for your character choice");
@@ -53,7 +53,7 @@ function writePassword() {
     var specialChars = confirm("Would you like special characters? Ok-Yes Cancel-No");
     var smallChars = confirm("Would you like lowercase letters? Ok-Yes Cancel-No");
     var bigChars = confirm("Would you like uppercase letters? Ok-Yes Cancel-No");
-    var inputNums = confirm("Would you like numebers in your password? Ok-Yes Cancel-No");
+    var inputNums = confirm("Would you like numbers in your password? Ok-Yes Cancel-No");
 
 
     if (!specialChars && !smallChars && !bigChars && !inputNums) {
@@ -83,17 +83,11 @@ for (var i = 0; i < inputCharacters; i++){
 
 console.log(userPassword);
 
-var password = userPassword.join('');
+var passwordDisplay = userPassword.join('');
 
-console.log(password);
-  //loop through userinput arrray based on how many characters they entered into the prompt
-
-  // check to make at least one of what they chose makes it into an array?
-
-  // push the randomly generated character to an empty array called password array
-
-  //return password array.join() --> makes it from an array into a string
-
+console.log(passwordDisplay);
+ 
+passwordText.value = passwordDisplay;
 
 };
 
